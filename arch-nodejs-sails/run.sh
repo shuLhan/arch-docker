70c8cb33a049197967f0348fc1adeb52770166f3
Mhd Sulhan <ms@kilabit.info>
2015-09-14 01:45:10 +0700

    Change image name.

--- arch-nodejs-sails/run.sh
+++ arch-nodejs-sails/run.sh
@@ -3,4 +3,4 @@
 SRC=$(pwd)/src
 
 ## run docker using host data directory as postgresql dictionary files.
-docker run -p 80:80 --rm --name proofn-api --link db-pg:db -v $SRC:/srv/www -it sulhan/proofn-api $@
+docker run -p 80:80 --rm --name sails --link db-pg:db -v $SRC:/srv/www -it sulhan/arch-nodejs-sails $@
