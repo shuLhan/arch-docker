7af083628cebc2772f4dcb3a2ddbb00c19ccff89
Mhd Sulhan <ms@kilabit.info>
2015-11-20 15:20:21 +0700

    arch-base-sailjs: on bootstraping, replace grunt with grunt-cli.

--- arch-base-sailsjs/bootstrap_sailsjs.sh
+++ arch-base-sailsjs/bootstrap_sailsjs.sh
@@ -2,7 +2,7 @@
 
 npm install -g sails --unsafe-perm && \
 	npm install -g pm2 --unsafe-perm && \
-	npm install -g grunt && \
+	npm install -g grunt-cli && \
 	rm -r /root/.node-gyp
 
 ## cleaning ...
