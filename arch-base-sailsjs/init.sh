ec3b2711318526c5a23187bc6491a527e7520a2d
Mhd Sulhan <ms@kilabit.info>
2015-11-21 13:22:48 +0700

    arch-base-sailsjs: set pm2 script to run pm2.json on source dir.

--- arch-base-sailsjs/init.sh
+++ arch-base-sailsjs/init.sh
@@ -2,7 +2,7 @@
 
 #COMMANDS="pm2 start app.js"
 WORKDIR=${WORKDIR:-"/srv/www"}
-COMMANDS=${COMMANDS:-"pm2 --no-daemon start app.js"}
+COMMANDS=${COMMANDS:-"pm2 --no-daemon start pm2.json"}
 NODE_ENV=${NODE_ENV:-"production"}
 
 cd $WORKDIR
