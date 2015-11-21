abaa798f061a33e86193f3f4037ddd1b8a7cc168
Mhd Sulhan <ms@kilabit.info>
2015-11-21 13:21:43 +0700

    arch-base-nodejs: add volume to mount host npm cache.
    Using host's npm cache can speeding up npm install when starting up
    the container.

--- arch-base-nodejs/run.sh
+++ arch-base-nodejs/run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-docker run --env-file=env --net=host --rm -v $PWD/src:/srv/www -it sulhan/arch-nodejs
+docker run --env-file=env --net=host --rm -v $PWD/npm:/root/.npm -v $PWD/src:/srv/www -it sulhan/arch-nodejs
