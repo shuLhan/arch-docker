26abb4fa20af2a7448b80323e28697a85f10414d
Mhd Sulhan <ms@kilabit.info>
2016-01-14 11:38:49 +0700

    redis: set port and volume on creating image.

--- arch-base-redis/run.sh
+++ arch-base-redis/run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-docker run --net=host --rm -v $PWD/data:/var/lib/redis -it sulhan/arch-redis
+docker run --net=host --rm -v $PWD/data.redis:/var/lib/redis -it sulhan/arch-redis
--- arch-base-redis/vars.sh
+++ arch-base-redis/vars.sh
@@ -4,6 +4,7 @@ THISD=${0:a:h}
 
 PKGS+=("redis")
 IMAGE_NAME="sulhan/arch-redis"
+IMAGE_ARGS=(-c="VOLUME /var/lib/redis" -c="EXPOSE 6379" -c="CMD /init.sh")
 
 FILES+=("${THISD}/init.sh" "${ROOTFS}/")
 FILES+=("${THISD}/redis.conf" "${ROOTFS}/etc/")
